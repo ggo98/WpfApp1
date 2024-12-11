964322b60ba790e9b18d19557f569c85b6ba195f
Olivier gg <ggo98@outlook.com>
2024-12-11 17:41:56 +0100

    feature-use-poorsql-formatter

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ using System.Windows.Navigation;
 using System.Xml;
 using System.Reflection;
 using ICSharpCode.AvalonEdit;
+using PoorMansTSqlFormatterLib.Interfaces;
+using PoorMansTSqlFormatterLib.Formatters;
 
 namespace WpfApp1
 {
@@ -37,10 +39,16 @@ namespace WpfApp1
 
             // Apply the syntax highlighting to the editor
             texteditor_SQL.SyntaxHighlighting = highlightingDefinition;
-
+            texteditor_SQL.Text = @"select N'Français ABCD abcd' French, N'Ελληνικά ΑΒΓΔ αβγδ' as Greek, N'עִברִית ﭏבגד' Hebrew, N'カタカナ_かたかな' Katakana_Hiragana,N'ひらがな_ヒラガナ ' Hiragana_Katakana,N'中文 月官匹刀' Chinese, N'عربى آ ب ي د' Arabic from datasetreport.demo.salesman";
             CreateContextMenu();
         }
 
+        private void HandleException(Exception ex)
+        {
+            string msg = ex.Message;
+            MessageBox.Show(msg);
+            Console.WriteLine(msg);
+        }
         private IHighlightingDefinition LoadHighlightingFromFile(string filePath)
         {
             // Ensure the file exists before trying to load it
@@ -63,39 +71,55 @@ namespace WpfApp1
         {
             MessageBox.Show("hello wpf");
 
-            //this.FontSize = 80;
-            //return;
+            //ChangeFontSize();
+            var sql = FormatSQL(texteditor_SQL.Text);
+            texteditor_SQL.SelectAll();
+            System.Windows.Clipboard.SetText(sql);
+            texteditor_SQL.Paste();
+            //texteditor_SQL.Text = sql;
 
 
-            foreach (var control in FindVisualChildren<Control>(this))
+        }
+
+        void ChangeFontSize()
+        {
+            try
             {
-                control.FontSize = 30;
-            }
-            return;
+                //this.FontSize = 80;
+                //return;
 
-            // marche pas...
-            var existingStyle = (Style)this.Resources["ControlStyle"];
-            if (existingStyle != null)
-            {
-                /*
-                var setter = new Setter(Control.FontSizeProperty, 30);
-                //System.InvalidOperationException: 'After a 'SetterBaseCollection' is in use (sealed), it cannot be modified.'
-                style.Setters.Clear();  // Clear existing setters
-                style.Setters.Add(setter);  // Add new setter for FontSize
-                */
-                var newStyle = new Style(typeof(Control)); // or target a specific type like Button or TextBox
-
-                // Copy the existing Setters (if needed) to preserve previous ones
-                foreach (var setter in existingStyle.Setters)
+                foreach (var control in FindVisualChildren<Control>(this))
                 {
-                    newStyle.Setters.Add(setter);
+                    control.FontSize = 30;
                 }
-                newStyle.Setters.Add(new Setter(Control.FontSizeProperty, 80));
+                return;
 
+                // marche pas...
+                var existingStyle = (Style)this.Resources["ControlStyle"];
+                if (existingStyle != null)
+                {
+                    /*
+                    var setter = new Setter(Control.FontSizeProperty, 30);
+                    //System.InvalidOperationException: 'After a 'SetterBaseCollection' is in use (sealed), it cannot be modified.'
+                    style.Setters.Clear();  // Clear existing setters
+                    style.Setters.Add(setter);  // Add new setter for FontSize
+                    */
+                    var newStyle = new Style(typeof(Control)); // or target a specific type like Button or TextBox
 
+                    // Copy the existing Setters (if needed) to preserve previous ones
+                    foreach (var setter in existingStyle.Setters)
+                    {
+                        newStyle.Setters.Add(setter);
+                    }
+                    newStyle.Setters.Add(new Setter(Control.FontSizeProperty, 80));
 
-                // Replace the existing style in the resources with the new one
-                this.Resources["ControlStyle"] = newStyle;
+                    // Replace the existing style in the resources with the new one
+                    this.Resources["ControlStyle"] = newStyle;
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
             }
         }
 
@@ -108,6 +132,10 @@ namespace WpfApp1
             var pasteMenuItem = new MenuItem { Header = "Paste", Command = ApplicationCommands.Paste };
             var deleteMenuItem = new MenuItem { Header = "Delete", Command = ApplicationCommands.Delete };
             var selectAllMenuItem = new MenuItem { Header = "Select All", Command = ApplicationCommands.SelectAll };
+            var formatSQLMenuItem = new MenuItem { Header = "Format SQL" };
+            formatSQLMenuItem.Click += FormatSQL_Click;
+            var undoMenuItem = new MenuItem { Header = "Undo", Command = ApplicationCommands.Undo };
+            var redoMenuItem = new MenuItem { Header = "Redo", Command = ApplicationCommands.Redo };
 
             contextMenu.Items.Add(cutMenuItem);
             contextMenu.Items.Add(copyMenuItem);
@@ -116,8 +144,76 @@ namespace WpfApp1
             contextMenu.Items.Add(deleteMenuItem);
             contextMenu.Items.Add(new Separator());
             contextMenu.Items.Add(selectAllMenuItem);
+            contextMenu.Items.Add(new Separator());
+            contextMenu.Items.Add(formatSQLMenuItem);
+            contextMenu.Items.Add(new Separator());
+            contextMenu.Items.Add(undoMenuItem);
+            contextMenu.Items.Add(redoMenuItem);
 
             texteditor_SQL.ContextMenu = contextMenu;
+        }
+
+        private void FormatSQL_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var sql = FormatSQL(texteditor_SQL.Text);
+                texteditor_SQL.SelectAll();
+                System.Windows.Clipboard.SetText(sql);
+                texteditor_SQL.Paste();
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        private string FormatSQL(string sql = "select * from a.b")
+        {
+            ISqlTokenizer _tokenizer;
+            ISqlTokenParser _parser;
+            ISqlTreeFormatter _formatter;
+
+            _tokenizer = new PoorMansTSqlFormatterLib.Tokenizers.TSqlStandardTokenizer();
+            _parser = new PoorMansTSqlFormatterLib.Parsers.TSqlStandardParser();
+
+            //var cfg = new SqlStandardFormatterOptions()// PoorMansTSqlFormatterLib.Formatters.TSqlStandardFormatterOptions
+            //{
+            //    IndentString = "\t",
+            //    SpacesPerTab = 4,
+            //    MaxLineWidth = 999,
+            //    ExpandCommaLists = true,
+            //    TrailingCommas = false,
+            //    SpaceAfterExpandedComma = false,
+            //    ExpandBooleanExpressions = true,
+            //    ExpandCaseStatements = true,
+            //    ExpandBetweenConditions = true,
+            //    ExpandInLists = true,
+            //    BreakJoinOnSections = false,
+            //    UppercaseKeywords = true,
+            //    HTMLColoring = true,
+            //    KeywordStandardization = false,
+            //    NewStatementLineBreaks = 1,
+            //    NewClauseLineBreaks = 1
+            //};
+            //cfg.HTMLColoring = false;
+            
+            _formatter = new TSqlStandardFormatter();
+
+            var tokenizedSql = _tokenizer.TokenizeSQL(sql);
+            var parsedSql = _parser.ParseSQL(tokenizedSql);
+            //PoorMansTSqlFormatterLib.Formatters.TSqlStandardFormatter stdFormatter = _parser as PoorMansTSqlFormatterLib.Formatters.TSqlStandardFormatter;
+            var formatter = (TSqlStandardFormatter)_formatter;
+
+            //var ret = formatter.FormatSQLTree(parsedSql, (FormatterCnDFlags)formatterCnDFlags); //, FormatterCnDFlags.Zero);
+            //var errOutputPrefix = _formatter.ErrorOutputPrefix;
+            //if (ret.StartsWith(errOutputPrefix))
+            //    ret = null;
+            //else
+            //    ret = FixStandardDateTimeFormattingIssue(ret);
+
+            var ret = formatter.FormatSQLTree(parsedSql);
+            return ret;
         }
 
         public static IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
